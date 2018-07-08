629668a5b7a6760d993a3d480e0509864b5bf449
albertocapriolo <alberto.capriolo@icloud.com>
2018-07-08 19:21:41 +0200

    Page admin ok.

--- src/app/component/admin-page/admin-page.component.ts
+++ src/app/component/admin-page/admin-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ModeAuction} from "../../class/ModeAuction";
-import {Customer} from "../../class/Customer";
-import {GetUserForAdminService} from "../../service/get-user-for-admin.service";
+import {ModeAuction} from '../../class/ModeAuction';
+import {Customer} from '../../class/Customer';
+import {GetUserForAdminService} from '../../service/get-user-for-admin.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -10,11 +10,22 @@ import {GetUserForAdminService} from "../../service/get-user-for-admin.service";
 })
 export class AdminPageComponent implements OnInit {
 
-  public users: Customer[];
+  users: Customer[];
+  animation = true;
+  error: String = null;
+  styleCard = String[5];
   constructor(private getUsersService: GetUserForAdminService) { }
 
   ngOnInit() {
-    this.getUsersService.getUsers().subscribe(data => this.users = data);
+    this.styleCard = [ 'bg-success', 'bg-danger', 'bg-warning', 'bg-info' ];
+    this.animation = true;
+    this.getUsersService.getUsers().subscribe(data => {
+      this.users = data;
+      this.animation = false;
+    }, (error: Error) => {
+      this.animation = false;
+      this.error = error.message;
+    });
   }
 
 }
